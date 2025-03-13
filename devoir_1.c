8140b99a518d31311b5d53cd5b711a3cf388c045
Gdecu <gaston.decumont@student.uclouvain.be>
2025-03-13 10:58:52 +0100

    1er commit

---
+++ devoir_1.c
@@ -0,0 +1,50 @@
+#include "devoir_1.h"
+#include <cblas.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define idxA(i, j) ((i)*n + (j))
+
+
+/**
+ * @brief tridiagonalise une matrice symétrique bande par transformations de similitudes
+ * @param A entrées de la matrice dans un tableau Row-Major :
+ *          - de taille n × n si [format] est full
+ *          - de taille n × (k + 1) si [format] est band
+ * @param n taille de la matrice
+ * @param k nombre de sous-diagonales non-nulles
+ * @param d tableau de taille n qui contient en sortie la diagonale principale de la matrice tridiagonale (Hessenberg symétrique)
+ * @param e est un tableau de taille n qui contient en sortie la sous-diagonale de la matrice tridiagonale dans ses n − 1 premiers éléments
+ */
+void tridiagonalize(double *A, int n, int k, double *d, double *e) {
+
+}
+
+
+/**
+ * @brief effectue une étape de l’algorithme QR avec un shift de Wilkinson µ sur une matrice tridiagonale symétrique
+ * @param d tableau de taille n qui contient la diagonale principale de la matrice tridiagonale, mis à jour en sortie de telle sorte que H_{k+1} ← Q^{∗}_{k} H_{k} Q_{k}
+ * @param e tableau de taille n qui contient la sous-diagonale de la matrice tridiagonale, mis à jour en sortie de telle sorte que H_{k+1} ← Q^{∗}_{k} H_{k} Q_{k}
+ * @param m indique la taille de la matrice active (m ≤ n), c’est-à-dire sur laquelle on n’a pas encore isolé les valeurs propres
+ * @param eps la tolérance pour déclarer qu’une valeur propre a été isolée : Golub et Van Loan proposent |hp+1,p| ≤ ϵ(|hpp| + |hp+1,p+1|).
+ * @return nombre d'itérations effectuées
+ */
+int step_qr_tridiag(double *d, double *e, int m, double eps) {
+    return 0;
+}
+
+/** 
+ * @brief calcule l’entièreté du spectre d’une matrice bande symétrique en faisant appel aux deux fonctions précedentes
+ * @param A entrées de la matrice dans un tableau Row-Major :
+ *          - de taille n × n si [format] est full
+ *          - de taille n × (k + 1) si [format] est band
+ * @param n taille de la matrice
+ * @param k nombre de sous-diagonales non-nulles
+ * @param eps la tolérance pour déclarer qu’une valeur propre a été isolée : Golub
+ * @param max_iter nombre maximal d’itérations qr qu'on souhaite effectuer
+ * @return retourne le nombre d’itérations nécessaires, ou bien -1 si l’algorithme n’a pas convergé
+*/
+int qr_eigs_(double *A, int n, int k, double eps, int max_iter) {
+    return 0;
+}
---
+++ devoir_1.h
@@ -0,0 +1,13 @@
+#ifndef CODE2_H
+#define CODE2_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+void tridiagonalize(double *A, int n, int k, double *d, double *e);
+int step_qr_tridiag(double *d, double *e, double m, double eps);
+int qr_eigs_(double *A, int n, int k, double eps, int max_iter);
+
+#endif
